1ea01799ea2cdafd6d22319668006b1fccd34bc2
Dustan Kraus <dustan.kraus@byu.edu>
2017-09-12 15:46:43 -0600

    working on plotting

--- deep_learning/lab2_perceptron/perceptron.py
+++ deep_learning/lab2_perceptron/perceptron.py
@@ -41,21 +41,28 @@ weight_norm_array = np.array([])
 num_outputs = 1 #since it's a binary problem (1 or 0)
 num_examples, num_features = np.shape(features)
 w = np.zeros([num_features + 1, num_outputs]) #the +1 is because I'm including the bias in the weights
+print type(w)
 x = np.concatenate((features.T,np.ones([1,num_examples])),axis=0) #stack  ones on to multiply the bias by
 z = np.sign(np.dot(w.T,x)).T #current perceptron classification output
 z = np.where(z < 0, 0, z) #change all the -1 to 0
-c = 0.1 #learning rate
+learning_rates = [0.01, 0.1, 1.0] #learning rate
 t = labels
 accuracy_array = np.append(accuracy_array, calc_classification_accuracy(w, z, t))
 
-for i in range(100):
-    del_w = c*np.dot((t-z).T,x.T)
-    w = w + del_w.T
-    z = np.sign(np.dot(w.T,x)).T #current perceptron classification output
-    z = np.where(z < 0, 0, z) #change all the -1 to 0
-    accuracy_array = np.append(accuracy_array,calc_classification_accuracy(w, z, t))
-    weight_norm_array = np.append(weight_norm_array,np.sqrt(np.dot(w.T,w)))
+for c in xrange(0,len(learning_rates)):
+    for i in range(100):
+        del_w = c*np.dot((t-z).T,x.T)
+        w = w + del_w.T
+        z = np.sign(np.dot(w.T,x)).T #current perceptron classification output
+        z = np.where(z < 0, 0, z) #change all the -1 to 0
+        accuracy_array = np.append(accuracy_array,calc_classification_accuracy(w, z, t))
+        weight_norm_array = np.append(weight_norm_array,np.sqrt(np.dot(w.T,w)))
 
-plt.figure()
-plt.plot(accuracy_array)
-plt.show()
+# plt.figure()
+# plt.plot(accuracy_array)
+# plt.plot(weight_norm_array)
+# plt.show()
+#
+# plt.figure()
+# plt.plot(weight_norm_array)
+# plt.show()
