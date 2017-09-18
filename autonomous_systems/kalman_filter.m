b72e26a47c38a4265001b596645730f79e5b2de6
Dustan Kraus <dustan.kraus@byu.edu>
2017-09-18 13:53:05 -0600

    finished kalman filter homework

--- autonomous_systems/kalman_filter.m
+++ autonomous_systems/kalman_filter.m
@@ -1,4 +1,8 @@
-% Vehicle Parameters
+clear all
+close all
+clc
+
+%Vehicle Parameters
 m = 100; %kg (Mass)
 b = 20; %N-s/m (Linear Drag Coeficcient)
 Ts = 0.05; %sample period in seconds
@@ -34,19 +38,78 @@ Q = 0.001; % Measurement Covariance (we are measuring position only)
 
 % Initial Conditions
 x0 = [0; 0];
-xt = zeros(2,1001); %true states
+xt = zeros(2,length(t)); %true states
 xt(:,1) = x0;
 
 % Simulate the System to get True States
 for i = 2:length(t)
-    xt(:,i) = A*xt(:,i-1) + B*u(i-1) + sqrt(R)*randn(2,1);
+    xt(:,i) = A*xt(:,i-1) + B*u(i-1) + R.^(0.5)*randn(2,1);
 end
+
+% Run Kalman Filter
+sigma_0 = eye(2); %initialize covariance
+mu_0 = [10*(rand()-0.5);5*(rand()-0.5)]; %initialize state estimate
+mu = zeros(2,length(t)); %vector for state estimates
+mu(:,1) = mu_0;
+sigma = zeros(2,2,length(t)); %vector for covariance
+sigma(:,:,1) = sigma_0;
+
+for i = 2:length(t)
+    % Get sensor measurement
+    z_t = C*xt(:,i) + Q.^(0.5)*randn(1);
+    [mu(:,i), sigma(:,:,i), K_t(:,i-1), sigma_bart] = kalman_filter_func(mu(:,i-1), sigma(:,:,i-1), u(i-1), z_t, A, B, C, R, Q);
+end
+K_t(:,length(t)) = K_t(:,length(t)-1);
 
 figure()
 plot(t,xt(1,:))
 hold on
 plot(t,xt(2,:))
+plot(t,mu(1,:))
+plot(t,mu(2,:))
+xlabel('Time (sec)')
+ylabel('State Magnitude')
+title('State and State Estimates')
+legend('Position (m)','Velocity (m/s)','Position Estimate (m)','Velocity Estimate (m/s)')
 
-% Run Kalman Filter
+figure()
+x_error = xt(1,:)-mu(1,:);
+xdot_error = xt(2,:)-mu(2,:);
+plot(t,x_error)
+hold on
+plot(t,xdot_error)
+%calculate mean and standard deviation of error to plot 95% confidence intervals
+x_err_mean = mean(x_error);
+x_err_std = std(x_error);
+xdot_err_mean = mean(xdot_error);
+xdot_err_std = std(xdot_error);
+x_upper = (x_err_mean + 2*x_err_std)*ones(length(t));
+x_lower = (x_err_mean - 2*x_err_std)*ones(length(t));
+xdot_upper = (xdot_err_mean + 2*xdot_err_std)*ones(length(t));
+xdot_lower = (xdot_err_mean - 2*xdot_err_std)*ones(length(t));
+plot(t,x_upper)
+plot(t,x_lower)
+plot(t,xdot_upper)
+plot(t,xdot_lower)
+xlabel('Time (sec)')
+ylabel('State Error Magnitude')
+title('Estimation Error')
+legend('Position Error (m)', 'Velocity Error (m/s)')
 
+figure()
+plot(t, reshape(sigma(1,1,:),length(t),1))
+hold on
+plot(t,reshape(sigma(2,2,:),length(t),1))
+xlabel('Time (sec)')
+ylabel('Error Covariance')
+title('Error Covariance vs. Time')
+legend('Position Covariance', 'Velocity Covariance')
 
+figure()
+plot(t, K_t(1,:))
+hold on
+plot(t,K_t(2,:))
+xlabel('Time (sec)')
+ylabel('Kalman Gains')
+title('Kalman Gains vs. Time')
+legend('Position gain', 'Velocity gain')
--- autonomous_systems/kalman_filter_func.m
+++ autonomous_systems/kalman_filter_func.m
@@ -1,7 +1,22 @@
-function [ output_args ] = kalman_filter_func( input_args )
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
+function [mu_t, sigma_t, K_t, sigma_bart] = kalman_filter_func(mu_tm1, sigma_tm1, u_t, z_t, A_t, B_t, C_t, R_t, Q_t)
+%KALMAN_FILTER_FUNC Calculates the kalman filter output
+%   INPUTS:     mu_tm1-     estimate of states at prior time step
+%               sigma_tm1-  estimate of covariance at prior time step
+%               u_t-        input
+%               z_t-        measurement
+%               A_t-        state space A matrix (discrete)
+%               B_t-        state space B matrix (discrete)
+%               C_t-        state space C matrix (discrete)
+%               R_t-        processs covariance
+%               Q_t-        measurement covariance
+%   OUTPUTS:    mu_t-       new estimate of states
+%               sigma_t-    new estimate of covariance
 
-
+mu_bart = A_t*mu_tm1 + B_t*u_t;
+sigma_bart = A_t*sigma_tm1*A_t' + R_t;
+K_t = sigma_bart*C_t'*inv(C_t*sigma_bart*C_t' + Q_t);
+% Measurement Update
+mu_t = mu_bart + K_t*(z_t - C_t*mu_bart);
+sigma_t = (eye(length(sigma_tm1)) - K_t*C_t)*sigma_bart;
 end
 
