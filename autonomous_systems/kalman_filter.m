e92f6ae5d963a0e3f136c44594d658bd8cbfbd7a
Dustan Kraus <dustan.kraus@byu.edu>
2017-09-22 10:31:07 -0600

    finished importing McLains data and running it

--- autonomous_systems/kalman_filter.m
+++ autonomous_systems/kalman_filter.m
@@ -8,7 +8,7 @@ b = 20; %N-s/m (Linear Drag Coeficcient)
 Ts = 0.05; %sample period in seconds
 
 % Set Up Input to System
-t = (0:0.05:50)';
+t = (0:Ts:50)';
 u = ones(1001, 1);
 for i = 1:length(t)
     if t(i) >= 0 && t(i) < 5
@@ -19,6 +19,8 @@ for i = 1:length(t)
         u(i) = 0;
     end
 end
+
+%u = sin(t);
 
 % Set up Continuous State Space System
 Ac = [0 1; 0 -b/m];
@@ -34,10 +36,12 @@ sys_d = c2d(sys_c, Ts);
 
 % Noise Characteristics
 R = [0.0001, 0; 0, 0.01]; % Process Covariance
+%R = [0.001, 0; 0, 0.1];
 Q = 0.001; % Measurement Covariance (we are measuring position only)
-
+%Q = 1;
 % Initial Conditions
 x0 = [0; 0];
+%x0 = [30;30];
 xt = zeros(2,length(t)); %true states
 xt(:,1) = x0;
 
@@ -56,9 +60,23 @@ sigma(:,:,1) = sigma_0;
 K_t = zeros(2,length(t));
 sigma_bart = zeros(2,2,length(t)-1);
 sigma_predict_update = zeros(2,2,2*length(t)-1);
+
+% Pull in McLain's test data for grading
+clear Q R sigma_0 mu_0 xt mu sigma
+load hw1_soln_data.mat %loads Q, R, Sig0
+R =  [R(2,2), 0; 0, R(1,1)];%his position and velocity states are in the opposite order as mine
+sigma_0 = [Sig0(2,2),0;0,Sig0(1,1)]; %his position and velocity states are in the opposite order as mine
+sigma = zeros(2,2,length(t)); %vector for covariance
+sigma(:,:,1) = sigma_0;
+mu = zeros(2,length(t)); %vector for state estimates
+mu_0 = [mu0(2);mu0(1)]; %his position and velocity states are in the opposite order as mine
+mu(:,1) = mu_0;
+xt = [xtr;vtr];
+
 for i = 2:length(t)
     % Get sensor measurement
-    z_t = C*xt(:,i) + Q.^(0.5)*randn(1);
+    %z_t = C*xt(:,i) + Q.^(0.5)*randn(1); %from true data with added noise
+    z_t = z(i);%from Mclain
     [mu(:,i), sigma(:,:,i), K_t(:,i-1), sigma_bart(:,:,i-1)] = kalman_filter_func(mu(:,i-1), sigma(:,:,i-1), u(i-1), z_t, A, B, C, R, Q);
 end
 K_t(:,length(t)) = K_t(:,length(t)-1);
@@ -88,35 +106,41 @@ legend('Position (m)','Velocity (m/s)','Position Estimate (m)','Velocity Estimat
 figure()
 x_error = xt(1,:)-mu(1,:);
 xdot_error = xt(2,:)-mu(2,:);
+subplot(2,1,1)
 plot(t,x_error)
 hold on
-plot(t,xdot_error)
-%calculate mean and standard deviation of error to plot 95% confidence intervals
 x_err_mean = mean(x_error);
 x_err_std = std(x_error);
-xdot_err_mean = mean(xdot_error);
-xdot_err_std = std(xdot_error);
 x_upper = (x_err_mean + 2*x_err_std)*ones(length(t));
 x_lower = (x_err_mean - 2*x_err_std)*ones(length(t));
+plot(t,x_upper,'r')
+plot(t,x_lower,'r')
+xlabel('Time (sec)')
+ylabel('Position Error (m)')
+
+subplot(2,1,2)
+plot(t,xdot_error)
+hold on
+xdot_err_mean = mean(xdot_error);
+xdot_err_std = std(xdot_error);
 xdot_upper = (xdot_err_mean + 2*xdot_err_std)*ones(length(t));
 xdot_lower = (xdot_err_mean - 2*xdot_err_std)*ones(length(t));
-plot(t,x_upper)
-plot(t,x_lower)
-plot(t,xdot_upper)
-plot(t,xdot_lower)
+plot(t,xdot_upper,'r')
+plot(t,xdot_lower,'r')
 xlabel('Time (sec)')
-ylabel('State Error Magnitude')
-title('Estimation Error')
-legend('Position Error (m)', 'Velocity Error (m/s)')
+ylabel('Velocity Error (m/s)')
+suptitle('Estimation Error')
 
 figure()
-plot(reshape(sigma_predict_update(1,1,:),length(sigma_predict_update),1))
-hold on
-plot(reshape(sigma_predict_update(2,2,:),length(sigma_predict_update),1))
+tt = 0:Ts/2:50;
+sig_pos = reshape(sigma_predict_update(1,1,:),length(sigma_predict_update),1);
+plot(tt(1:200),sig_pos(1:200))
+%hold on
+%plot(reshape(sigma_predict_update(2,2,:),length(sigma_predict_update),1))
 xlabel('Time Step')
-ylabel('Error Covariance')
+ylabel('Covariance (m^2)')
 title('Error Covariance vs. Time Step')
-legend('Position Covariance', 'Velocity Covariance')
+legend('Position Covariance')
 
 figure()
 plot(t, K_t(1,:))
