cba75b552143464d3d59a3ac6e1b59a59a1b71e4
Dustan Kraus <dustan.kraus@byu.edu>
2017-10-03 14:25:16 -0600

    modified a weight

--- autonomous_systems/hw3_ukf/unscented_kalman_filter.m
+++ autonomous_systems/hw3_ukf/unscented_kalman_filter.m
@@ -24,7 +24,7 @@ function [mu_t, sigma_t, Kt] = unscented_kalman_filter(mu_tm1, sigma_tm1, u_tm1,
     
     %Generate sigma points (2L + 1)
     R = chol(sigma_a_tm1); % matrix square root
-    alpha = 1; %scaling parameter to determine how spread out sigma points are
+    alpha = .1; %scaling parameter to determine how spread out sigma points are
     kappa = 3; %scaling parameter to determine how spread out sigma points are
     lamda = alpha^2 * (n + kappa) - n;
     gamma = sqrt(n+lamda);
