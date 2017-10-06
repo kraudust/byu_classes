d429a4d224831bcf2b43ec1320f77eabe506a188
kraudust <dustan.kraus@gmail.com>
2017-10-05 23:35:54 -0600

    might have gotten it working by adding more layers, and changing some dimensions, gonna run it overnight

--- deep_learning/lab5_tensorflow_image_classifier/lab5_image_classifier.py
+++ deep_learning/lab5_tensorflow_image_classifier/lab5_image_classifier.py
@@ -1,7 +1,8 @@
 import tensorflow as tf
 import numpy as np
-import pdb
-
+import matplotlib.pyplot as plt
+# import pdb
+#----------------------------------CONVOLUTION FUNCTION-------------------------------------------------------
 def conv( x, filter_size=3, stride=1, num_filters=64, is_output=False, name="conv" ):
         '''
         x is an input tensor (4 dimensional)
@@ -23,9 +24,9 @@ def conv( x, filter_size=3, stride=1, num_filters=64, is_output=False, name="con
             if not is_output:
                 conv_out = tf.nn.relu(conv_bias,name='relu')
                 return conv_out
-            if is_output:
+            else:
                 return conv_bias
-
+#---------------------------------Fully Connected Function ---------------------------------------------------
 def fc(x, out_size=50, is_output=False, name="fc"):
         '''
         x is an input tensor
@@ -46,9 +47,9 @@ def fc(x, out_size=50, is_output=False, name="fc"):
             if not is_output:
                 fc_out = tf.nn.relu(y,name='relu')
                 return fc_out
-            if is_output:
+            else:
                 return y
-
+#---------------------------------------Reshape Function------------------------------------------------------
 def reshape_cifar_image(im):
     im_new = np.zeros((1,32,32,3))
     l = 0
@@ -84,29 +85,30 @@ labels3 = data3['labels']
 labels4 = data4['labels']
 labels5 = data5['labels']
 labels_test = data_test['labels']
-labels1 = np.atleast_2d(labels1).T #size is number of samples x 1
-labels2 = np.atleast_2d(labels2).T #size is number of samples x 1
-labels3 = np.atleast_2d(labels3).T #size is number of samples x 1
-labels4 = np.atleast_2d(labels4).T #size is number of samples x 1
-labels5 = np.atleast_2d(labels5).T #size is number of samples x 1
-labels_test = np.atleast_2d(labels_test).T #size is number of samples x 1
+# pdb.set_trace()
+# labels1 = np.atleast_2d(labels1).T #size is number of samples x 1
+# labels2 = np.atleast_2d(labels2).T #size is number of samples x 1
+# labels3 = np.atleast_2d(labels3).T #size is number of samples x 1
+# labels4 = np.atleast_2d(labels4).T #size is number of samples x 1
+# labels5 = np.atleast_2d(labels5).T #size is number of samples x 1
+# labels_test = np.atleast_2d(labels_test).T #size is number of samples x 1
 features_train = np.concatenate((features1, features2, features3, features4, features5),axis=0)
 labels_train = np.concatenate((labels1, labels2, labels3, labels4, labels5), axis=0)
 
-# pdb.set_trace()
 #-----------------------------------Build Neural Net ---------------------------------------------------------
 #define placeholder for data and labels
 input_image = tf.placeholder(tf.float32,shape=[1,32,32,3],name='image')
 lab = tf.placeholder(np.int64,shape=[], name = 'label')
 
 #define the neural net which outputs scores
-h0 = conv(input_image,name='conv1')
-h1 = conv(h0,name='conv2')
-h2 = conv(h1,stride = 2, name='conv3')
+h0 = conv(input_image,filter_size=7, num_filters=128, name='conv1')
+h1 = conv(h0,filter_size = 5, num_filters = 64, name='conv2')
+h2 = conv(h1,stride = 2, num_filters= 32,name='conv3')
 h2_shape = h2.get_shape().as_list()
 h2_flat = tf.reshape(h2, [h2_shape[1]*h2_shape[2]*h2_shape[3], -1])
-fc0 = fc(h2_flat,out_size = 25,name='fc1')
-score = fc(fc0, out_size = 10,is_output = True, name = 'fc2')
+fc0 = fc(h2_flat,out_size = 100,name='fc1')
+fc1 = fc(fc0, out_size =50, name='fc2')
+score = fc(fc1, out_size = 10,is_output = True, name = 'fc3')
 
 #determine loss
 with tf.name_scope('softmax_cross_entropy_loss'):
@@ -119,7 +121,7 @@ with tf.name_scope('accuracy'):
 
 #Optimizer
 with tf.name_scope('optimizer'):
-    train_weights = tf.train.AdamOptimizer(learning_rate=1e-4).minimize(loss)
+    train_weights = tf.train.AdamOptimizer(learning_rate=1e-5).minimize(loss)
 #-------------------------------------------------------------------------------------------------------------
 
 init = tf.global_variables_initializer()
@@ -130,20 +132,29 @@ tf.summary.scalar('loss', loss)
 merged = tf.summary.merge_all()
 accuracy_array = np.array([])
 for i in range(len(labels_train)):
+# for i in range(2000):
     im = reshape_cifar_image(features_train[i,:])
-    sess.run(train_weights, feed_dict = {input_image:im, lab:labels_train[i,0]})
-    ss = sess.run(merged, feed_dict = {input_image:im, lab:labels_train[i,0]})
-    writer.add_summary(ss,i)
-    # print i
+    sess.run(train_weights, feed_dict = {input_image:im, lab:labels_train[i]})
     if i % 100.0 == 0: #check accuracy every 100 training images
+        ss = sess.run(merged, feed_dict = {input_image:im, lab:labels_train[i]})
+        writer.add_summary(ss,i)
+        print 'Loss: ', sess.run(loss,feed_dict = {input_image:im, lab:labels_train[i]})
         acc_sum = 0.0
         test_indices = np.random.choice(len(labels_test),size=200)
         for j in range(200): #test accuracy on 200 random images from the test set
-            acc_i = sess.run(acc, feed_dict = {input_image: reshape_cifar_image(features_test[test_indices[j],:]), lab: labels_test[test_indices[j],0]})
+            acc_i = sess.run(acc, feed_dict = {input_image: reshape_cifar_image(features_test[test_indices[j],:]), lab: labels_test[test_indices[j]]})
             acc_sum = acc_sum + acc_i
         accuracy = (acc_sum/200.0)*100.0
-        print accuracy
-        print i
+        print 'Accuracy: ', accuracy
+        print 'i: ', i, 'out of ', len(labels_train)
+        print '\n'
         accuracy_array = np.append(accuracy_array,accuracy)
 writer.close()
+
+plt.figure()
+plt.plot(accuracy_array)
+plt.xlabel('Epochs')
+plt.ylabel('Classification Accuracy %')
+plt.title('Accuracy')
+plt.show()
 
