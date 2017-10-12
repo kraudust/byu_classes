03979a3603b4bf504795e9fc0d11cd3bfc86fefa
Dustan Kraus <dustan.kraus@byu.edu>
2017-10-12 15:14:10 -0600

    reshaped some stuff, still using all my ram and swap

--- deep_learning/lab6_cancer_detector/lab6_cancer_detector.py
+++ deep_learning/lab6_cancer_detector/lab6_cancer_detector.py
@@ -61,9 +61,10 @@ neg_test_filenames = os.listdir('/home/kraudust/git/personal_git/byu_classes/dee
 neg_test_labels  = os.listdir('/home/kraudust/git/personal_git/byu_classes/deep_learning/lab6_cancer_detector/cancer_data/outputs/test/neg')
 
 # n = 300 #n*2 = number of training images to use
-n = 10
+n = 4
 train_index = random.sample(range(len(pos_train_filenames)),n)
-im_size = 512
+# im_size = 512
+im_size = 256
 train_ims = np.zeros((2*n,im_size,im_size,3)).astype(np.float32)
 train_labs = np.zeros((2*n,im_size,im_size,1)).astype(np.float32)
 test_ims = np.zeros((175,im_size,im_size,3)).astype(np.float32)
@@ -84,7 +85,7 @@ for i in xrange(n):
 
 #load test data into matrices
 # for i in xrange(len(neg_test_filenames)):
-for i in xrange(10):
+for i in xrange(n):
     if i < 75:
         test_im_pos = skio.imread(data_dir + 'inputs/test/pos/' + pos_test_filenames[i])
         test_im_neg = skio.imread(data_dir + 'inputs/test/neg/' + neg_test_filenames[i])
@@ -111,22 +112,27 @@ batch_size = 2
 input_images = tf.placeholder(tf.float32,[batch_size,im_size,im_size,3],name='image')
 label_images = tf.placeholder(tf.int64,[batch_size, im_size, im_size],name = 'label')
 #define the neural net
-l0 = conv(input_images, name='conv0')
-l1 = conv(l0, name = 'conv1')
+l0 = conv(input_images, name='conv0', num_filters = 16)
+l1 = conv(l0, name = 'conv1', num_filters = 16)
 # l2 = tf.nn.max_pool(l1,ksize = [1,2,2,1], strides = [1,2,2,1],padding = 'VALID', name = 'l2')
 l2 = max_pool(l1, name = 'pool2')
-l3 = conv(l2, name = 'conv3', num_filters = 128)
-l4 = conv(l3, name = 'conv4', num_filters = 128)
-l5 = upconv(l4, name="upconv5")
-l6 = conv(l5, name = 'conv6')
-l7 = tf.concat([l1, l6], 3, name='concat')
-l8 = conv(l7,name = 'conv8')
-l9 = conv(l8,name = 'conv9')
-score = conv(l9,name = 'conv10', num_filters = 1,filter_size = 1 , is_output = True)
+l3 = conv(l2, name = 'conv3', num_filters = 32)
+l4 = conv(l3, name = 'conv4', num_filters = 32)
+l5 = upconv(l4, name="upconv5", output_channels = 16)
+l6 = tf.concat([l1, l5], 3, name='concat')
+l7 = conv(l6, name = 'conv6', num_filters = 16)
+l8 = conv(l7,name = 'conv8', num_filters = 8)
+score = conv(l8,name = 'conv9', num_filters = 2, is_output = True)
+output_image = tf.argmax(score,3)
+# score = conv(l9,name = 'conv10', num_filters = 1,filter_size = 2 , is_output = True)
 
 #calculate loss
 with tf.name_scope('softmax_cross_entropy_loss'):
     loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels=label_images, logits = score))
+
+#calculate accuracy
+with tf.name_scope('accuracy'):
+    accuracy = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(score,3),label_images),tf.float32))
 
 #Optimizer
 with tf.name_scope('optimizer'):
@@ -139,10 +145,12 @@ sess.run(init)
 writer = tf.summary.FileWriter("./tf_logs",sess.graph)
 train_im = np.reshape(train_ims[0:batch_size,:,:,:],[batch_size,im_size,im_size,3])
 train_lab = np.reshape(train_labs[0:batch_size,:,:,:],[batch_size,im_size,im_size]).astype(np.int64)
-print np.rank(train_lab)
-# sess.run(l2, feed_dict = {input_images:train_im, label_images:train_lab})
-for i in range(30):
-    sess.run(train_step, feed_dict = {input_images: train_im, label_images: train_lab})
-    print sess.run(loss, feed_dict = {input_images: train_im, label_images: train_lab})
+
+print "finished initializing neural net..."
+# for i in range(1):
+    # print "running train step ", i
+print sess.run(loss, feed_dict = {input_images: train_im, label_images: train_lab})
+print sess.run(accuracy, feed_dict = {input_images: train_im, label_images: train_lab})
+    # print "loss ", sess.run(loss, feed_dict = {input_images: train_im, label_images: train_lab})
 writer.close()
 
