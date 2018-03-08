8fb91d4015250c3e2d15b3e131166ee9af940cc8
Dustan Kraus <dustan.kraus@byu.edu>
2018-03-08 15:23:00 -0700

    visual odometry sucks

--- robotic_vision/hw6_visual_odometry/visual_odometry.py
+++ robotic_vision/hw6_visual_odometry/visual_odometry.py
@@ -19,6 +19,8 @@ class visual_odom():
         # z - out the top
         # states
         self.init_plots = True
+        self.R_cam_quad = np.array([[0.,0.,-1.],[1., 0., 0.], [0., -1., 0.]])
+        self.get_first_states = True
         self.orientation = None
         self.phi = 0.0
         self.th = 0.0
@@ -28,7 +30,8 @@ class visual_odom():
         self.position = None
         self.state = {} # states dictionary containing velocity, orientation, position, and imu
         self.flag = 0
-        self.env = Holodeck.make("UrbanCity")
+        # self.env = Holodeck.make("UrbanCity")
+        self.env = Holodeck.make("EuropeanForest")
         self.command = np.array([0.0, 0.0, 0.0, 0.0]) # roll, pitch, yaw rate, altitude
         self.state, reward, terminal, _ = self.env.step(self.command)
 
@@ -79,18 +82,21 @@ class visual_odom():
         self.th = np.arcsin(-self.orientation[2,0]) # pitch
         self.phi = np.arcsin(self.orientation[2,1]/np.cos(self.th)) # roll
         self.psi = np.arcsin(self.orientation[1,0]/np.cos(self.th)) # yaw
+        if self.get_first_states == True:
+            self.position_est = deepcopy(self.position)
+            self.get_first_states = False
 
     def calc_optical_flow(self):
         # Calculate new points that grids have moved to
         self.p1, st, err = cv2.calcOpticalFlowPyrLK(self.gray_old, self.gray_cur, self.p0, None, **self.lk_params)
 
         # Select points where optical flow exists (i.e. inverse existed)
-        good_new = self.p1[st==1]
-        good_old = self.p0[st==1]
+        self.good_new = self.p1[st==1]
+        self.good_old = self.p0[st==1]
 
         # draw the tracks for visualization
         mask = np.zeros_like(self.image)
-        for j,(new,old) in enumerate(zip(good_new,good_old)):
+        for j,(new,old) in enumerate(zip(self.good_new,self.good_old)):
             a,b = new.ravel()
             c,d = old.ravel()
             mask = cv2.line(mask, (a,b),(c,d), [0,0,255], 1)
@@ -103,22 +109,75 @@ class visual_odom():
         # Calculate new features to track in current image
         self.p0 = cv2.goodFeaturesToTrack(self.gray_old, mask = None, **self.feature_params)
 
+    def compute_visual_odom(self):
+        # E, mask = cv2.findEssentialMat(self.good_new, self.good_old, focal=1.0, method=cv2.RANSAC, prob=0.999, threshold=1.0)
+        E, mask = cv2.findEssentialMat(self.good_new, self.good_old, focal=1.0, method=cv2.RANSAC, prob=0.999, threshold=0.1)
+        R1, R2, t = cv2.decomposeEssentialMat(E)
+        if np.trace(R1) > 2.5:
+            R = deepcopy(R1)
+        else:
+            R = deepcopy(R2)
+
+        #checks that x2*E*x1 = 0 and x2*that*R*x1 = 0
+        # that = np.array([[0, -t[2], t[1]], [t[2], 0, -t[0]], [-t[1], t[0], 0]])
+        # x2 = np.append(self.good_new[0,:], 1).reshape(3,1)
+        # x1 = np.append(self.good_old[0,:], 1).reshape(3,1)
+        # print(np.matmul(x2.T, np.matmul(that, np.matmul(R, x1))))
+        # print(np.matmul(x2.T, np.matmul(E, x1)), '\n')
+
+        # Rpos = np.matmul(R, self.orientation)
+        # self.position_est = self.position_est + np.matmul(t, Rpos)
+        # diff =  np.matmul(self.orientation.T, np.matmul(self.R_cam_quad,t*np.linalg.norm(self.velocity)/10.))
+        # diff[0] = -diff[0]
+
+
+        self.position_est = self.position_est + np.matmul(self.orientation, np.matmul(self.R_cam_quad,t*np.linalg.norm(self.velocity)/10.))
+        # self.position_est = self.position_est + diff 
+
+        # print(R)
+        # print(t)
+        # print(np.linalg.norm(t))
+
     def plot_states(self):
         if self.init_plots == True:
+            self.i = 0
             self.app = pg.QtGui.QApplication([])
             self.ekfplotwin = pg.GraphicsWindow(size=(800,400))
             self.ekfplotwin.setWindowTitle('Position')
             self.ekfplotwin.setInteractive(True)
             self.plots = self.ekfplotwin.addPlot(1,1)
-            self.xycurves = self.plots.plot(pen=(0,0,255))
+            self.plots2 = self.ekfplotwin.addPlot(2,1)
+            self.plots3 = self.ekfplotwin.addPlot(3,1)
+            self.xcurves = self.plots.plot(pen=(0,0,255))
+            self.xcurves_est = self.plots.plot(pen=(0,255,0))
+            self.ycurves = self.plots2.plot(pen=(0,0,255))
+            self.ycurves_est = self.plots2.plot(pen=(0,255,0))
+            self.zcurves = self.plots3.plot(pen=(0,0,255))
+            self.zcurves_est = self.plots3.plot(pen=(0,255,0))
             self.xdata = []
+            self.xdata_est = []
             self.ydata = []
+            self.ydata_est = []
+            self.zdata = []
+            self.zdata_est = []
+            self.time = []
             self.init_plots = False
         else:
             self.xdata.append(self.position[0,0])
             self.ydata.append(self.position[1,0])
-            self.xycurves.setData(self.xdata, self.ydata)
+            self.zdata.append(self.position[2,0])
+            self.xdata_est.append(self.position_est[0,0])
+            self.ydata_est.append(self.position_est[1,0])
+            self.zdata_est.append(self.position_est[2,0])
+            self.time.append(self.i)
+            self.xcurves.setData(self.time, self.xdata)
+            self.xcurves_est.setData(self.time, self.xdata_est)
+            self.ycurves.setData(self.time, self.ydata)
+            self.ycurves_est.setData(self.time, self.ydata_est)
+            self.zcurves.setData(self.time, self.zdata)
+            self.zcurves_est.setData(self.time, self.zdata_est)
         self.app.processEvents()
+        self.i += 1
 
 
     def send_commands(self,events):
@@ -152,6 +211,7 @@ class visual_odom():
                 self.get_states() # get current states
                 if frame == 3: # do optical flow on every 3rd frame
                     self.calc_optical_flow() # calculate optical flow
+                    self.compute_visual_odom()
                     frame = 0
                 self.plot_states()
                 events = pygame.event.get() # get keyboard input
