b3e3d67a857eaa123efa2adaa59af79ef4236519
Dustan Kraus <dustan.kraus@byu.edu>
2018-04-03 16:23:39 -0600

    looked at recursive lease squares

--- math_of_signals_and_systems/Homeworks/HW_6/hw6.m
+++ math_of_signals_and_systems/Homeworks/HW_6/hw6.m
@@ -20,10 +20,10 @@ y = a*t.^2 + b.*t + c  + d*sin(t) + 5*randn(1,length(t))';
 %Pm = eye(3,3);
 Pm = eye(n,n);
 %x_m = zeros(3,1);
-x_m = zeros(n,1);
+x_m = zeros(n,1)+50;
 x_store(1,:) = x_m;
 
-for i = 1:length(t),
+for i = 1:length(t)
     am1 = [t(i)^2; t(i); 1; sin(t(i))];
     bm1 = y(i);
     Km1 = (Pm*am1)/(1+am1'*Pm*am1);
@@ -40,7 +40,8 @@ xlabel('t (sec)')
 ylabel('y')
 hold on
 three_vec = x_m(3)*ones(length(t),1);
-plot(t,x_m(1)*t.^2 + x_m(2).*t + three_vec + x_m(4)*sin(t))
+% plot(t,x_m(1)*t.^2 + x_m(2).*t + three_vec + x_m(4)*sin(t))
+plot(t,x_store(1:end-1,1).*t.^2 + x_store(1:end-1,2).*t + x_store(1:end-1,3) + x_store(1:end-1,4).*sin(t))
 legend('True Data', 'RLS Estimate')
 
 figure()
